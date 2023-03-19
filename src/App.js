b258c8b68afef610d47b1244219c7c23b162fce2
John Paul Schmidt <TheJohnPaulSchmidt@gmail.com>
2023-03-19 14:44:44 -0500

    Nav add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Home from './Home';
+
 import './App.css';
 
-function App() {
+const NotFound = () => {
+  return <h2>404 Not Found</h2>;
+}
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav class="navbar fixed-left navbar-expand-md navbar-light">
+      <a class="navbar-brand raleway" href="#">J.P.S. Portfolio</a> 
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse form-inline" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto form-inline">
+          <li class="nav-item active raleway">
+            <a class="nav-link" href="#about-me">About Me</a>
+          </li>
+          <li class="nav-item active raleway">
+            <a class="nav-link" href="#social-media">Social Media</a>
+          </li>
+          <li class="nav-item active raleway">
+            <a class="nav-link" href="#projects">Projects</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
