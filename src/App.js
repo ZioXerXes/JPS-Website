4994304e0dac7312b4d64e5a350cc0fcd829d71f
John Paul Schmidt <TheJohnPaulSchmidt@gmail.com>
2023-03-19 18:25:37 -0500

    Quittin' time

--- src/App.js
+++ src/App.js
@@ -10,61 +10,61 @@ const NotFound = () => {
 const App = () => {
   return (
     <Router>
-      <nav class="navbar fixed-left navbar-expand-md navbar-light">
-      <a class="navbar-brand raleway" href="#">J.P.S. Portfolio</a> 
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <nav className="navbar fixed-left navbar-expand-md navbar-light">
+      <a className="navbar-brand raleway" href="#">J.P.S. Portfolio</a> 
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse form-inline" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto form-inline">
-          <li class="nav-item active raleway">
-            <a class="nav-link" href="#about-me">About Me</a>
+      <div className="collapse navbar-collapse form-inline" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto form-inline">
+          <li className="nav-item active raleway">
+            <a className="nav-link" href="#about-me">About Me</a>
           </li>
-          <li class="nav-item active raleway">
-            <a class="nav-link" href="#social-media">Social Media</a>
+          <li className="nav-item active raleway">
+            <a className="nav-link" href="#social-media">Social Media</a>
           </li>
-          <li class="nav-item active raleway">
-            <a class="nav-link" href="#projects">Projects</a>
+          <li className="nav-item active raleway">
+            <a className="nav-link" href="#projects">Projects</a>
           </li>
         </ul>
       </div>
     </nav>
-    <div class="container">
-      <div class="row main">
-        <div class="col-7">
-          <div class="nameplate">
+    <div className="container">
+      <div className="row main">
+        <div className="col-7">
+          <div>
             <h1>John Paul Schmidt</h1>
-            <h2>Writer and Coder for hire.</h2>
-            <div class="button-box" id="social-media">
-              <button class="social-button button-left">
+            <h2 className="text-center">Writer / Coder</h2>
+            <div className="button-box text-center" id="social-media">
+              <button className="social-button button-left">
                 <a href="https://www.linkedin.com/in/john-paul-schmidt-b1306941" target="#">
-                  <div class="button-contents">
-                    <i class="fab fa-linkedin" id="icon"></i>
-                    <p class="button-label">LinkedIn</p>
+                  <div className="button-contents">
+                    <i className="fab fa-linkedin" id="icon"></i>
+                    <p className="button-label">LinkedIn</p>
                   </div>
                 </a>
               </button>
-              <button class="social-button button-right">
+              <button className="social-button button-right">
                 <a href="https://www.github.com/ZioXerXes" target="#">
-                  <div class="button-contents">
-                    <i class="fab fa-github" id="icon"></i>
-                    <p class="button-label">GitHub</p>
+                  <div className="button-contents">
+                    <i className="fab fa-github" id="icon"></i>
+                    <p className="button-label">GitHub</p>
                   </div>
                 </a>
               </button>
             </div>
           </div>
         </div>
-        <div class="col-5">
-          <img class="picture" src={profilePic} alt="profilePic" />
+        <div className="col-5">
+          <img className="picture" src={profilePic} alt="profilePic" />
         </div>
       </div>
       <hr/>
     </div>
-    <div class="footer container">
-      <div class="row">
-        <div class="col-12">
+    <div className="footer container">
+      <div className="row">
+        <div className="col-12">
         </div>
       </div>
     </div>
